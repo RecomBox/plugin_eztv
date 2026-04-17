6238119b6be3165f7a35ebfbe9cc6de66ce16721
GoodDay360 <istartgame31@gmail.com>
2026-04-18 06:04:23 +0700

    fix: invalid seeeds check

--- src/get_torrents.ts
+++ src/get_torrents.ts
@@ -5,6 +5,7 @@ import request from "@plugin_provider/method/request";
 export default async function get_torrents(input_payload: get_torrents_types.InputPayload): Promise<get_torrents_types.OutputPayload> {
 
     let new_output_payload: get_torrents_types.OutputPayload = [];
+    let seeds: number[] = [];
 
     let id_obj = JSON.parse(input_payload.id);
     let imdb_id = id_obj["imd_id"];
@@ -23,7 +24,6 @@ export default async function get_torrents(input_payload: get_torrents_types.Inp
         let data = res.body_json();
         let result_list = data["torrents"]??[];
 
-        console.log(result_list.length);
 
         if (result_list.length == 0) break;
 
@@ -36,17 +36,28 @@ export default async function get_torrents(input_payload: get_torrents_types.Inp
             if (
                 (parseInt(result["season"]) !== s)
                 || (parseInt(result["episode"]) !== e)
-                || (parseInt(result["seeds"]) !== 0)
+                || (parseInt(result["seeds"]) === 0)
             ) continue;
             new_output_payload.push({
                 title: result["title"]??"",
                 torrent_url: result["magnet_url"]??"",
             })
+            seeds.push(parseInt(result["seeds"] ?? "0"));
+            console.log(result["seeds"] ?? 0);
         }
         current_page++;
 
         if (new_output_payload.length >= 100) break;
     };
+
+    let sorted_indices = seeds
+    .map((val, idx) => ({ val, idx }))
+    .sort((a, b) => b.val - a.val)
+    .map(obj => obj.idx);
+
+    let sorted_payload: get_torrents_types.OutputPayload = sorted_indices.map(
+        idx => new_output_payload[idx]!
+    );
     
-    return new_output_payload;
+    return sorted_payload;
 }
